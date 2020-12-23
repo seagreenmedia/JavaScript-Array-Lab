83bbb682e3a2576330966a6e4b4977865192a9af
seagreenmedia <erinjenkins57@gmail.com>
2020-12-23 16:36:37 -0500

    closer

--- ArrayUtils.js
+++ ArrayUtils.js
@@ -1,44 +1,70 @@
 class ArrayUtils {
 
-    let ArrayUtils = ["Peace", "Love", "Light"];
-
-    //isEmpty
-    let isEmpty = [];
-    isEmpty === ArrayUtils;
-
-    //append
-    ArrayUtils.push("Freedom");
-
-    //clone
-    identity = (x) => x;
-    ArrayUtilsCopy = ArrayUtils.map(identity);
-
-    //subArray
-    let y = +prompt("Pick a number between 0 and 2");
-    let z = +prompt("Pick another number between 0 and 2");
-    subArray = ArrayUtils.slice(y, z + 1);
-
-    //equals
-    function arraysEqual(arr1, arr2) {
-        if (arr1 === arr2)
-            return true;
-        if (arr1 == null || arr2 == null)
-            return false;
-        if (arr1.length !== arr2.length)
-            return false;
+    constructor(original, array, value) {
+        this.original = original;
+        this.array = array;
+        this.value = value;
     }
 
-    //fill
-    ArrayUtils.fill(prompt("What's your favorite color?"));
+    isEmpty(array) {
+        if (array[""]) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 
-    //indexOf
-    ArrayUtils.indexOf("Love");
+    append(value) {
+        return original.push(value);
+    }
 
-    //remove
+    getClone() {
+        let identity = (original) => original
+        let ArrayUtilsCopy = ArrayUtils.map(identity);
+        return ArrayUtilsCopy;
+    }
 
-    return ArrayUtils;
-    remove(original, value); { return original; };
+    getSubArray(y, z) {
+        let y
+        let z
+        let subArray = ArrayUtils.slice(y, z + 1);
+        return subArray;
+    }
 
+    arraysEqual(arr1, arr2) {
+        if (arr1 === arr2)
+            return true;
+        else if (arr1 == null || arr2 == null)
+            return false;
+        else(arr1.length !== arr2.length)
+        return false;
+    }
 
-    //reverse
-    reverse(original); {};
+    fillArray(original, value) {
+        return ArrayUtils.fill(value);
+    }
+
+    indexOf(original, value) {
+        return ArrayUtils.indexOf(value);
+    }
+
+    remove(original, value) {
+        let copy = stats;
+        for (let i = 0; i < copy.length; i++) {
+            if (copy[i] === value) {
+                copy.splice(i, 1);
+            }
+        }
+        let value = 6;
+        stats.slice(value);
+        console.log(stats);
+    }
+
+    reverseArray([original]) {
+        let newArray = [];
+        for (let i = original.length - 1; i >= 0; i--) {
+            newArray += original.charAt([i]);
+        }
+        return newArray;
+    }
+}
